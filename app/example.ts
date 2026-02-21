bb0c8fee433fa7201c690b0cf4f317cf350e2099
abdelrahmen1313 <abdelrahmenbenfarhat@gmail.com>
2026-02-21 06:00:58 +0100

    adding publish workflow

--- app/example.ts
+++ app/example.ts
@@ -1,4 +1,4 @@
-import { fetchPageMetadata } from "./page-metadata";
+import { fetchPageMetadata } from "./page-metadata.js";
 const data = await fetchPageMetadata("https://medium.com/@pshubham/using-react-with-cordova-f235de698cc3");
 console.log(data);
 //https://fr.wikipedia.org/wiki/Ski_acrobatique_aux_Jeux_olympiques_de_2026
